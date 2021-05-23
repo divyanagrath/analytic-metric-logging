e3db2fa89480f71f4b63323702bcca4c61ad7f4c
divyanagrath <d.nagrath23@gmail.com>
2021-05-23 18:40:09 +0530

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -5,23 +5,20 @@ const AutoLoad = require('fastify-autoload')
 const fastify = require('fastify')(config.fastify)
 const path = require('path')
 
-async() => {
-await fastify.register(AutoLoad, {
+module.exports = fastify;
+fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
     options: Object.assign({})
 })
-await fastify.register(AutoLoad, {
+fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'routes'),
     options: Object.assign({ prefix: config.apiPrefix })
 })
 
-await fastify.listen(config.port, '0.0.0.0', (err, address) => {
+fastify.listen(config.port, '0.0.0.0', (err, address) => {
     if (err) {
         fastify.log.error(err)
         process.exit(0)
     }
     console.log(`Server listening on ${address}`)
 })
-
-module.exports = fastify;
-}
